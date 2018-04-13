d761bb182b224d453ed2c69b939e799464eede4c
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-13 13:13:26 +0300

    CyclicRotation

--- CyclicRotation/Program.cs
+++ CyclicRotation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CyclicRotation
 {
@@ -6,7 +7,28 @@ namespace CyclicRotation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine(string.Join(",", new Solution().solution(new int[] { }, 3)));
+        }
+    }
+
+    class Solution
+    {
+        public int[] solution(int[] A, int K)
+        {
+            LinkedList<int> nums = new LinkedList<int>(A);
+
+            if(nums.Count > 0)
+            {
+                for (int i = 0; i < K; i++)
+                {
+                    LinkedListNode<int> last = nums.Last;
+
+                    nums.RemoveLast();
+                    nums.AddFirst(last);
+                }
+            }
+
+            return nums.ToArray();
         }
     }
 }
---
+++ ReverseArrayExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseArrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(string.Join(" ", Reverse(array)));
+        }
+
+        static int[] Reverse(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                int temp = array[i];
+                int k = n - i - 1;
+                array[i] = array[k];
+                array[k] = temp;
+            }
+
+            return array;
+        }
+    }
+}
