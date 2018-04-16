be048ef7e606090b38a1a07d16e5a8cfe9336428
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-16 14:22:53 +0300

    TapeEquilibrium

---
+++ CountExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CountExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConstantTime(5));
+        }
+
+        static int Slow(int n)
+        {
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+
+        static int Fast(int n)
+        {
+            int result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result += (i + 1);
+            }
+
+            return result;
+        }
+
+        static int ConstantTime(int n)
+        {
+            int result = n * (n + 1) / 2;
+
+            return result;
+        }
+    }
+}
--- TapeEquilibrium/Program.cs
+++ TapeEquilibrium/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TapeEquilibrium
 {
@@ -6,7 +7,44 @@ namespace TapeEquilibrium
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine(new Solution().solution(new int[] { 3, 1, 2, 4, 3 }));
+            Console.WriteLine(new Solution().solution(new int[] { 0, 0, 20 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] nums)
+        {
+            int minDiff = int.MaxValue;
+            
+            int sumOfAll = nums.Sum();
+
+            int currentSum = nums[0];
+            int rest = sumOfAll - currentSum;
+
+            int currentDiff = Math.Abs(currentSum - rest);
+
+            if(currentDiff < minDiff)
+            {
+                minDiff = currentDiff;
+            }
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                int num = nums[i];
+
+                currentSum += num;
+                rest = sumOfAll - currentSum;
+                currentDiff = Math.Abs(currentSum - rest);
+
+                if (currentDiff < minDiff)
+                {
+                    minDiff = currentDiff;
+                }
+            }
+
+            return minDiff;
         }
     }
 }
