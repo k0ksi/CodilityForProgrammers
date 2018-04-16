1b5852a246993e97694c87f1ec8c102e398ca809
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-16 14:27:44 +0300

    FrogJmp

--- FrogJmp/Program.cs
+++ FrogJmp/Program.cs
@@ -6,7 +6,30 @@ namespace FrogJmp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(10, 85, 30));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            //int start = X;
+            //bool reached = false;
+            //int counter = 0;
+            //while(true)
+            //{
+            //    if(reached)
+            //    {
+            //        break;
+            //    }
+
+
+            //}
+
+            decimal preciseResult = (decimal)(Y - X) / (decimal)D;
+
+            return (int)Math.Ceiling(preciseResult);
         }
     }
 }
