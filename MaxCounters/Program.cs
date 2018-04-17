51d51c89829f3e5e7694c9a0acffd575e9290384
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-17 17:45:13 +0300

    maxCounters task

--- MaxCounters/Program.cs
+++ MaxCounters/Program.cs
@@ -6,7 +6,47 @@ namespace MaxCounters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(string.Join(" ", new Solution().solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 })));
+        }
+    }
+
+    class Solution
+    {
+        public int[] solution(int N, int[] A)
+        {
+            int[] counter = new int[N];
+            int max = -1;
+            int currentMin = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] >= 1 && A[i] <= N)
+                {
+                    if (counter[A[i] - 1] < currentMin)
+                        counter[A[i] - 1] = currentMin;
+
+                    counter[A[i] - 1] = counter[A[i] - 1] + 1;
+
+                    if(counter[A[i] - 1] > max)
+                    {
+                        max = counter[A[i] - 1];
+                    }
+                }
+                else if(A[i] == N + 1)
+                {
+                    currentMin = max;
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if(counter[i] < currentMin)
+                {
+                    counter[i] = currentMin;
+                }
+            }
+
+            return counter;
         }
     }
 }
