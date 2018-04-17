90d5a6ee8dd899b340b6209d2856cee882936be5
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-17 10:04:42 +0300

    permmissingelement

--- PermMissingElem/Program.cs
+++ PermMissingElem/Program.cs
@@ -6,7 +6,33 @@ namespace PermMissingElem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { 2, 3, 1, 5 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            bool[] found = new bool[A.Length + 2];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                found[A[i]] = true;
+            }
+
+            int result = 0;
+
+            for (int i = 1; i < found.Length; i++)
+            {
+                if(found[i] == false)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return 1;
         }
     }
 }
