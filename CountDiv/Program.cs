21be3470e60725e4e81600102d14ad64bb97a7b8
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-18 15:19:33 +0300

    countDiv and prefixSumsExercise

--- CountDiv/Program.cs
+++ CountDiv/Program.cs
@@ -6,7 +6,22 @@ namespace CountDiv
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(6, 11, 2));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int A, int B, int K)
+        {
+            int offsetForLeftRange = 0;
+
+            if (A % K == 0)
+            {
+                ++offsetForLeftRange;
+            }
+
+            return B / K - A / K + offsetForLeftRange;
         }
     }
 }
---
+++ PrefixSumsExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PrefixSumsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrefixSums(new int[] { 1, 2, 3 }));
+            Console.WriteLine(TotalOfOneSliceCount(new int[] {1,2,3,4,5}, 1, 3));
+        }
+
+        static int[] PrefixSums(int[] A)
+        {
+            int n = A.Length;
+            int[] P = new int[n + 1];
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                P[i] = P[i - 1] + A[i - 1];
+            }
+
+            return P;
+        }
+
+        static int[] SuffixSums(int[] A)
+        {
+            int n = A.Length;
+            int[] P = new int[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                P[i] = P[i - 1] + A[A.Length - i];
+            }
+
+            return P;
+        }
+
+        static int TotalOfOneSliceCount(int[] P, int x, int y)
+        {
+            return P[y + 1] - P[x];
+        }
+    }
+}
