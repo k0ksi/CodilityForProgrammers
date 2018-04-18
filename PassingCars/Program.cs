3c6a057e7153e8f8de2f6b1b5f0fc5234ee2437d
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-18 15:40:38 +0300

    passingCars

--- PassingCars/Program.cs
+++ PassingCars/Program.cs
@@ -6,7 +6,55 @@ namespace PassingCars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { 0, 1, 0, 1, 1 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int count = 0;
+
+            // TODO
+            int zeroesFound = 0;
+            int firstZeroIndex = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int num = A[i];
+
+                if(num == 0)
+                {
+                    zeroesFound = 1;
+                    firstZeroIndex = i;
+
+                    break;
+                }
+            }
+
+            if(firstZeroIndex < A.Length)
+            {
+                for (int i = firstZeroIndex + 1; i < A.Length; i++)
+                {
+                    int num = A[i];
+
+                    if (num == 0)
+                    {
+                        zeroesFound++;
+                    }
+                    else
+                    {
+                        count += zeroesFound;
+                        if (count > 1000000000)
+                        {
+                            return -1;
+                        }
+                    }
+                }
+            }
+
+            return count;
         }
     }
 }
