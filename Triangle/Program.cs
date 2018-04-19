9f6bb261bc7c1dd70ee78f5f698c1d0921f2f291
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-19 15:32:41 +0300

    triangle

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -6,7 +6,45 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { int.MinValue, int.MinValue, 2, 5, 1, 8, 20 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+
+            int result = 0;
+
+            Array.Sort(A);
+
+            if(n < 3)
+            {
+                return result;
+            }
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    int p = A[i - 2];
+                    int q = A[i - 1];
+                    int r = A[i];
+
+                    long firstSum = (long)p + (long)q;
+                    long secondSum = (long)q + (long)r;
+                    long thirdSum = (long)r + (long)p;
+
+                    if (firstSum > r && secondSum > p && thirdSum > q)
+                    {
+                        result = 1;
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
