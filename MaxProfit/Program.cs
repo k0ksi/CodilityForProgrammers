5e63b763927382d07088bfde5e31fe4fb45471a4
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-25 16:01:24 +0300

    MaxProfit

--- MaxProfit/Program.cs
+++ MaxProfit/Program.cs
@@ -6,7 +6,46 @@ namespace MaxProfit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { 23171, 21011, 21123, 21366, 21013, 21367 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int minPossible = int.MaxValue;
+            int currentMin = 0;
+            int currentProfit = 0;
+            int maxProfit = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int current = A[i];
+                currentMin = Math.Max(0, current);
+
+                if(i != 0)
+                {
+                    currentProfit = currentMin - minPossible;
+                }
+
+                if(currentMin < minPossible)
+                {
+                    minPossible = currentMin;
+                }
+
+                if(currentMin > minPossible)
+                {
+                    currentProfit = currentMin - minPossible;
+
+                    if(currentProfit > maxProfit)
+                    {
+                        maxProfit = currentProfit;
+                    }
+                }
+            }
+
+            return maxProfit;
         }
     }
 }
