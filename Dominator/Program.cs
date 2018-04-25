7ec48ee656454a852127b0a1b761f8750607e0cb
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-25 12:43:25 +0300

    dominator

--- Dominator/Program.cs
+++ Dominator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dominator
 {
@@ -6,7 +7,66 @@ namespace Dominator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { 3, 4, 3, 2, 3, -1, 3, 3 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = A[i];
+
+                if (stack.Count == 0)
+                {
+                    stack.Push(A[i]);
+                }
+                else
+                {
+                    if (stack.Peek() != A[i])
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(A[i]);
+                    }
+                }
+            }
+
+            int candidate = -1;
+
+            if (stack.Count > 0)
+            {
+                candidate = stack.Peek();
+            }
+
+            int leader = -1;
+            int count = 0;
+            int index = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == candidate)
+                {
+                    index = i;
+                    count++;
+                }
+            }
+
+            if (count > n / 2)
+            {
+                leader = candidate;
+                return index;
+            }
+
+            return leader;
         }
     }
 }
