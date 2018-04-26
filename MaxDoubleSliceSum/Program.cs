1984d78b1937bdb93dd4ea32fd9e6618b374ede2
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-26 10:19:22 +0300

    maxDoubleSliceSum

--- MaxDoubleSliceSum/Program.cs
+++ MaxDoubleSliceSum/Program.cs
@@ -6,7 +6,36 @@ namespace MaxDoubleSliceSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(new int[] { 3, 2, 6, -1, 4, 5, -1, 2 }));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+            int[] k1 = new int[n];
+            int[] k2 = new int[n];
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                k1[i] = Math.Max(k1[i - 1] + A[i], 0);
+            }
+
+            for (int i = n - 2; i > 0; i--)
+            {
+                k2[i] = Math.Max(k2[i + 1] + A[i], 0);
+            }
+
+            int max = 0;
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                max = Math.Max(max, k1[i - 1] + k2[i + 1]);
+            }
+
+            return max;
         }
     }
 }
