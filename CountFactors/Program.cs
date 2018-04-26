7a1cba5aa2fba965895a644ea50a1264e4cfe78c
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-04-26 13:35:26 +0300

    CountFactors

--- CountFactors/Program.cs
+++ CountFactors/Program.cs
@@ -6,7 +6,30 @@ namespace CountFactors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Solution().solution(24));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int N)
+        {
+            int numFactors = 0;
+            for (int i = 1; i <= (double)Math.Sqrt(N); i++)
+            {
+                if (N % i == 0)
+                {
+                    if (i * i != N)
+                    {
+                        numFactors = numFactors + 2;
+                    }
+                    else if (i * i == N)
+                    {
+                        numFactors = numFactors + 1;
+                    }
+                }
+            }
+            return numFactors;
         }
     }
 }
---
+++ DivisorsDemoExercise/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace DivisorsDemoExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReversingCoins(10));
+        }
+
+        static int CountDivisors(int n)
+        {
+            int i = 1;
+            int result = 0;
+
+            while (i * i < n)
+            {
+                if(n % i == 0)
+                {
+                    result += 2;
+                }
+
+                i += 1;
+            }
+
+            if(i * i == n)
+            {
+                result += 1;
+            }
+
+            return result;
+        }
+
+        static bool PrimalityTest(int n)
+        {
+            int i = 2;
+            while (i * i <= n)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+
+                i++;
+            }
+
+            return true;
+        }
+
+        static int ReversingCoins(int n)
+        {
+            int result = 0;
+            var coins = new int[n + 1];
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                int k = i;
+                while (k <= n)
+                {
+                    coins[k] = (coins[k] + 1) % 2;
+                    k += i;
+                }
+
+                result += coins[i];
+            }
+
+            return result;
+        }
+    }
+}
